ef108254cc8af1a9dcdfab1dc173fe1d939198bf
ann grynchuk <ann.grynchuk7@gmail.com>
2019-01-03 13:11:11 +0200

    add inhabitants of a tiny JS world

--- index.js
+++ index.js
@@ -1,31 +1,63 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
+   Code repository:https://github.com/AnnaGrynchuk/a-tiny-JS-world
    Web app: _put project's github pages URL here_
    */
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+const dog = {
+    species: 'dog',
+    name: 'Toby',
+    gender: 'male',
+    legs: 4,
+    hands: 0,
+    saying: 'woof-woof!'
+  };
 
+const cat = {
+    species: 'cat',
+    name: 'Persik',
+    gender: 'male',
+    legs: 4,
+    hands: 0,
+    saying: 'meawww-meaww!'
+}
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+const woman = {
+    species: 'human',
+    name: 'Poly',
+    gender: 'female',
+    legs: 2,
+    hands: 2,
+    saying: 'I am hungry!'
+}
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const man = {
+    species: 'human',
+    name: 'Alex',
+    gender: 'male',
+    legs: 2,
+    hands: 2,
+    saying: 'Lets go to bar!'
+}
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+print("In a tiny JS world lives " + dog.species + ". Its name is <strong>" +  dog.name  + "</strong>. Its gender is " + dog.gender + ". It has strong " + 
+  dog.legs + " legs and " + dog.hands + " hands. When it wants to play it says " + dog.saying );
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+print("Wow! We see fluffy " + cat.species + " in our JS world. Its name is <strong>" +  cat.name  + "</strong>. Its gender is " + cat.gender + ". It has long " + 
+  cat.legs + " legs and " + cat.hands + " hands. At 4a.m it always says " + cat.saying );
+
+print("Suddenly beautiful " + woman.species + " appears. Her name is <strong>" +   woman.name  + "</strong>. Her gender is " +  woman.gender + ". She has long " + 
+woman.legs + " legs and thin " +  woman.hands + " hands. She said to her friend " +  woman.saying );
+
+print("Her friend <strong> " + man.name  + " </strong> wasn't surprised, he knows her well. He has strong  " +  man.hands + " hands and  " +  man.legs + " legs so he easily picked up her and said " +  man.saying );
+
+woman.saying ="I'm hungry too!";
+let newMan = Object.assign({},woman);
+
+print(newMan.saying);
+
 
 
